b1dbae1114670a14fd5220a9b95a424bd27aae15
Hasindri <59213918+Hasindri@users.noreply.github.com>
2021-07-30 04:10:10 +0530

    updated RNN code

--- RNN_training.py
+++ RNN_training.py
@@ -1,49 +1,27 @@
 
+from numpy import loadtxt, savetxt
+import random
 import tensorflow as tf
 import numpy as np
-from keras.models import Model
-from keras.layers import Input, LSTM, Dense
+from tensorflow.keras import layers
+from tensorflow.keras.models import Model, Sequential
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
 import os
-from keras.layers import LeakyReLU
-from scipy import ndimage
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
-import numpy as np
-import matplotlib.pyplot as plt
-import PIL
-from PIL import Image
-from keras.models import Sequential
-from keras.layers import Dense, Activation, Flatten, Input
-from keras.layers import Conv2D, MaxPooling2D, UpSampling2D, Reshape,BatchNormalization
-import matplotlib.pyplot as plt
-from keras import backend as K
-import numpy as np
-from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
-from keras.models import Model
-import keras
-from keras.models import model_from_json
-from keras.models import Sequential
-from keras.layers import Dense, Activation, Flatten, Input,Dropout
-from keras.layers import Conv2D, MaxPooling2D, UpSampling2D
-import matplotlib.pyplot as plt
-from keras import backend as K
-import numpy as np
-from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
-from keras.layers import BatchNormalization,MaxPooling3D, Reshape,RNN,LSTM,SimpleRNN
-from keras.layers import Concatenate,TimeDistributed
-from keras.models import Model
 import models
 import utils
 
-os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID";
- 
+
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+
 # The GPU id to use, usually either "0" or "1";
-os.environ["CUDA_VISIBLE_DEVICES"]="1";
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
 # print('Number of devices: {}'.format(strategy.num_replicas_in_sync))
+
+
 def lr_schedule(epoch):
- 
+
     lr = 1e-3
     if epoch > 50:
         lr *= 0.1
@@ -56,24 +34,24 @@ def lr_schedule(epoch):
 dataset_x = []
 dataset_y = []
 
-data_x,data_y = utils.Preprocessor()
+data_x, data_y = utils.Preprocessor('data')
 dataset_x.extend(data_x)
 dataset_y.extend(data_y)
 
 dataset_x = np.array(dataset_x)
 dataset_y = np.array(dataset_y)
 #print(dataset_x )
-print ("Dataset X shape",dataset_x.shape)
-print ("Dataset Y shape",dataset_y.shape)
+print("Dataset X shape", dataset_x.shape)
+print("Dataset Y shape", dataset_y.shape)
 
 len_ = dataset_x.shape[0]
-len_test = len_//10
+print('len', len_)
+len_test = len_//5
+print('len test', len_test)
 len_valid = len_test//5
+print('len valid', len_valid)
 
-import random
-from numpy import loadtxt,savetxt
-
-numbers = np.array(random.sample(range(1196), 1196))
+numbers = np.array(random.sample(range(len_), len_))
 savetxt('numbers_44.csv', numbers, delimiter=',')
 
 test = numbers[0:len_test]
@@ -91,69 +69,72 @@ final_test_y = test_y[len_valid:]
 train_x = dataset_x[train]
 train_y = dataset_y[train]
 
-print ("Dataset X shape",train_x.shape)
-print ("Dataset Y shape",train_y.shape)
+print("Dataset X shape", train_x.shape)
+print("Dataset Y shape", train_y.shape)
 
-print ("Testing X shape",final_test_x.shape)
-print ("Testing Y shape",final_test_y.shape)
+print("Testing X shape", final_test_x.shape)
+print("Testing Y shape", final_test_y.shape)
 
-print ("Validation X shape",valid_x.shape)
-print ("Validation Y shape",valid_y.shape)
+print("Validation X shape", valid_x.shape)
+print("Validation Y shape", valid_y.shape)
+
 
 #####################################################################################################################
-#---------------------------------ENCODER----------------------------------------------------------------------------
+# ---------------------------------ENCODER----------------------------------------------------------------------------
 #####################################################################################################################
 
-encoder =  models.Encoder_Gene()
-rms = keras.optimizers.Adam(lr=0.001)
+encoder = models.Encoder_Gene()
+rms = tf.keras.optimizers.Adam(lr=0.001)
 encoder.compile(optimizer=rms, loss='mse')
 
 
-#Load the pretrained encoder weights here
-encoder.load_weights("/content/AE_weights/encoder.h5")
-encoder.trainable=False
-rms = keras.optimizers.Adam(lr=0.001)
+# Load the pretrained encoder weights here
+encoder.load_weights("AE_weights/encoder.h5")
+encoder.trainable = False
+rms = tf.keras.optimizers.Adam(lr=0.001)
 encoder.compile(optimizer=rms, loss='mse')
-encoder.summary()
+# encoder.summary()
 
-modelpre =  models.Time_dis_ENC(encoder)
-modelpre.trainable=False
-rms = keras.optimizers.Adam(lr=0.001)
+modelpre = models.Time_dis_ENC(encoder)
+modelpre.trainable = False
+rms = tf.keras.optimizers.Adam(lr=0.001)
 modelpre.compile(optimizer=rms, loss='mse')
 
-modelpre.summary()
+# modelpre.summary()
+
+
 #####################################################################################################################
-#---------------------------------DECODER----------------------------------------------------------------------------
+# ---------------------------------DECODER----------------------------------------------------------------------------
 #####################################################################################################################
 
-#load the pretrained decoder weights here
-decoder =models.Decoder_Gene()
-decoder.load_weights("/content/AE_weights/decoder.h5")
-decoder.trainable=False
-rms = keras.optimizers.Adam(lr=0.001)
+# load the pretrained decoder weights here
+decoder = models.Decoder_Gene()
+decoder.load_weights("AE_weights/decoder.h5")
+decoder.trainable = False
+rms = tf.keras.optimizers.Adam(lr=0.001)
 decoder.compile(optimizer=rms, loss='mse')
-decoder.summary()
+# decoder.summary()
 
 #####################################################################################################################
-#---------------------------------MIDDLE LAYER----------------------------------------------------------------------------
+# ---------------------------------MIDDLE LAYER----------------------------------------------------------------------------
 #####################################################################################################################
 
 
-#Dense layer and output a RELU 1024-1024 dense layer. Add 
-modelmid =models.Middle()
-rms = keras.optimizers.Adam(lr=0.001)
+# Dense layer and output a RELU 1024-1024 dense layer. Add
+modelmid = models.Middle()
+rms = tf.keras.optimizers.Adam(lr=0.001)
 modelmid.compile(optimizer=rms, loss='mse',
-              metrics=['accuracy'])
-modelmid.summary()
+                 metrics=['accuracy'])
+# modelmid.summary()
 
 
-#SUPERMODEL
-inp1 = layers.Input((3,128, 128, 1),name='Initial_three_images')
-inp2 = layers.Input((1,1024),name='Zero_Tensor')
+# SUPERMODEL
+inp1 = layers.Input((3, 128, 128, 1), name='Initial_three_images')
+inp2 = layers.Input((1, 1024), name='Zero_Tensor')
 
 x = modelpre(inp1)
-x = modelmid([x,inp2])
-x = Reshape((4, 4, 64),name='Reshaping')(x)
+x = modelmid([x, inp2])
+x = layers.Reshape((4, 4, 64), name='Reshaping')(x)
 output = decoder(x)
 
 
@@ -161,23 +142,23 @@ output = decoder(x)
 #modelmid.name = "Seq2Seq_LSTM"
 #decoder.name ="Convolutional_Decoder"
 
-generator = Model(inputs=[inp1,inp2],outputs=output)
-rms = keras.optimizers.Adam(lr=0.001)
+generator = Model(inputs=[inp1, inp2], outputs=output)
+rms = tf.keras.optimizers.Adam(lr=0.001)
 generator.compile(optimizer=rms, loss='mse')
 
 generator.summary()
 
 
-lr_scheduler = keras.callbacks.LearningRateScheduler(lr_schedule)
+lr_scheduler = tf.keras.callbacks.LearningRateScheduler(lr_schedule)
 
 
-
-generator.fit([train_x,np.zeros((train_x.shape[0],1,1024))],train_y,
-            epochs=50,
-            batch_size=1,
-            shuffle=True,
-            validation_data=([valid_x,np.zeros((valid_x.shape[0],1,1024))],valid_y),
-            callbacks=[lr_scheduler])
+generator.fit([train_x, np.zeros((train_x.shape[0], 1, 1024))], train_y,
+              epochs=50,
+              batch_size=5,
+              shuffle=True,
+              validation_data=(
+                  [valid_x, np.zeros((valid_x.shape[0], 1, 1024))], valid_y),
+              callbacks=[lr_scheduler])
 
 modelmid.save_weights("AE_weights/tanh_seq2seq.h5")
-print ('Weights Saved')
+print('Weights Saved')
